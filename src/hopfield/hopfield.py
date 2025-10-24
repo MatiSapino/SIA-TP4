9bec36d3057c7a5dceb556f17bccb908414303e5
tobias <tpugliano@itba.edu.ar>
2025-10-24 17:34:05 -0300

    fix: modified break condition to prevent the Hopfield network from oscilate

--- src/hopfield/hopfield.py
+++ src/hopfield/hopfield.py
@@ -4,6 +4,9 @@ import numpy as np
 def _initialize_weights(data: np.ndarray) -> np.ndarray:
     w = data.T @ data #uso la formula de la diapositiva 29. Pongo primero la transpuesta pues lo que en la teorica es K aca es data.T
     np.fill_diagonal(w, 0)
+    print(data)
+    print(data.T)
+    print(data.shape)
     return w / data.shape[1] # data.shape devuelve la tupla (P , N) donde N es el numero de neuronas y P el numero de patrones almacenados.
 
 
@@ -17,11 +20,16 @@ class Hopfield:
     def train(self, pattern: np.ndarray) -> np.ndarray:
         self.neurons = pattern.copy()
         prev_state = np.zeros_like(self.neurons)
+        prev_prev_state = np.zeros_like(self.neurons)
         iteration = 0
-        while not np.array_equal(prev_state, self.neurons):
+        while not np.array_equal(prev_state, self.neurons) and not np.array_equal(prev_prev_state, self.neurons):
+            prev_prev_state = prev_state.copy()
             prev_state = self.neurons.copy()
             print(f"Iteration {iteration}: Neuron States: {self.neurons}")
             self.neurons = np.sign(self.weights @ self.neurons)
+            for i in range(len(self.neurons)):
+                if self.neurons[i] == 0:
+                    self.neurons[i] = prev_state[i]
             iteration += 1
 
         return self.neurons
